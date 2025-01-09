ca5f7733883896f47c60badfcbd8dc1bd63453d9
ARamirezCaballero~ <ramirezcaballeroamparo@gmail.com>
2025-01-09 14:15:37 -0300

    fixing errors again

--- src/main/java/com/mindhub/todolist/dtos/TaskDTO.java
+++ src/main/java/com/mindhub/todolist/dtos/TaskDTO.java
@@ -13,11 +13,12 @@ public class TaskDTO {
     public TaskDTO() {}
 
     // Constructor with fields
-    public TaskDTO(Long id, String title, String description, TaskStatus status, Long aLong) {
+    public TaskDTO(Long id, String title, String description, TaskStatus status, Long userId) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.status = status;
+        this.userId = userId;
     }
 
     // Getters and Setters
@@ -62,10 +63,12 @@ public class TaskDTO {
     }
 
     public Long getUserId() {
+
         return userId;
     }
 
     public void setUserId(Long userId) {
+
         this.userId = userId;
     }
 }
